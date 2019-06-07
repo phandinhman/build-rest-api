a0e71db9851abf9ac137c67032ffef0599d1e2ae
phan.dinh.man <FRAMGIA\phan.dinh.man@b120606-pc.framgia.com>
2019-06-07 16:08:17 +0700

    Add mux

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 	"log"
 	"encoding/json"
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id string `json:Id`
 	Title string `json:"Title"`
 	Desc string `json:"Desc"`
 	Content string `json:"Content"`
 }
+
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Homepage!")
@@ -20,18 +24,32 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: articles")
-	Articles := make([]Article, 2)
-	Articles[0] = Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"}
-	Articles[1] = Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"}
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func singleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", singleArticle)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API")
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequest()
 }
